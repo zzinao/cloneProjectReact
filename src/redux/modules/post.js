1d4761f2d4a608da7ae73e8d430bffb15a113c9f
hyoungwonkang <hwplus@gmail.com>
2022-04-17 11:54:29 +0900

    CRUD 중 D빼고 나머지 구현

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,4 +1,3 @@
-//커밋
 import axios from 'axios';
 import produce from 'immer';
 
@@ -7,11 +6,13 @@ import { createAction, handleActions } from 'redux-actions';
 //액션
 const ADD_POST = 'ADD_POST';
 const GET_POST = 'GET_POST';
+const GETONE_POST = 'GETONE_POST';
 const EDIT_POST = 'EDIT_POST';
 
 //액션 생성
 const addPost = createAction(ADD_POST, (post) => ({ post }));
 const getPost = createAction(GET_POST, (posts) => ({ posts }));
+const getOnePost = createAction(GETONE_POST, (post) => ({ post }));
 const editPost = createAction(EDIT_POST, (postNum, post) => ({
   postNum,
   post,
@@ -20,6 +21,7 @@ const editPost = createAction(EDIT_POST, (postNum, post) => ({
 //초기값
 const initialState = {
   list: [],
+  detail: [],
 };
 
 //미들웨어
@@ -31,15 +33,11 @@ const addPostDB = (formData) => {
     };
     await axios({
       method: 'post',
-      url: 'https://reqres.in/api/users',
-      //   url: 'api/post'
-      data: {
-        name: 'morpheus',
-        job: 'leader',
-      },
-      //    formData,
+      url: 'https://95bdde78-b028-4db6-b139-9f7e8f06b2e5.mock.pstmn.io/post',
+      data: formData,
       headers: {
         'Content-Type': 'multipart/form-data',
+        // Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     })
       .then((res) => {
@@ -57,14 +55,29 @@ const addPostDB = (formData) => {
 const getPostDB = () => {
   return async function (dispatch, getState, { history }) {
     await axios
-      //   .get("/api/posts?postNum=1")
-      .get('https://reqres.in/api/unknown')
+      .get('/api/posts?postNum=1')
+      // .get('https://reqres.in/api/unknown')
       .then((res) => {
         let _posts = [];
         res.data.posts.forEach((posts) => {
           _posts.push({ num: posts.num, ...posts });
         });
         dispatch(getPost(_posts));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
+
+//포스트 및 디테일
+const getOnePostDB = (id) => {
+  return async function (dispatch, getState, { history }) {
+    await axios
+      .get(`/api/posts?postNum=1`)
+      .then((res) => {
+        let post = res.data.detail;
+        dispatch(getOnePost(post));
       })
       .catch((err) => {
         console.log(err);
@@ -90,8 +103,8 @@ const editPostDB = (postNum, formData) => {
 
     await axios({
       method: 'post',
-      //   url: "/api/posts?postNum=1",
-      url: 'https://reqres.in/api/users/2',
+      url: '/api/posts?postNum=1',
+      // url: 'https://reqres.in/api/users/2',
       data: formData,
       headers: {
         'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
@@ -118,14 +131,26 @@ export default handleActions(
     [GET_POST]: (state, action) =>
       produce(state, (draft) => {
         draft.list = draft.list.reduce((acc, cur) => {
-          if (acc.findIndex((a) => a.id === cur.id) === -1) {
+          if (acc.findIndex((a) => a.num === cur.num) === -1) {
             return [...acc, cur];
           } else {
-            acc[acc.findIndex((a) => a.id === cur.id)] = cur;
+            acc[acc.findIndex((a) => a.num === cur.num)] = cur;
             return acc;
           }
         }, []);
         draft.list = action.payload.posts;
+      }),
+    [GETONE_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.detail = action.payload.post;
+      }),
+    [EDIT_POST]: (state, action) =>
+      produce(state, (draft) => {
+        let index = draft.list.findIndex(
+          (p) => p.num === action.payload.postNum
+        );
+        console.log(index, '인덱스는?');
+        draft.list[index] = { ...draft.list[index], ...action.payload.post };
       }),
   },
   initialState
@@ -138,6 +163,8 @@ const actionCreators = {
   getPostDB,
   editPost,
   editPostDB,
+  getOnePost,
+  getOnePostDB,
 };
 
 export { actionCreators };
