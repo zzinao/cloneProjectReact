679f0fb3f3cc4230528f669ed535a17927411de6
hyoungwonkang <hwplus@gmail.com>
2022-04-17 11:55:36 +0900

    Detail페이지와 Post컴포넌트 확인용 루트 추가

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,9 @@
-//커밋
 import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import { history } from '../redux/configureStore';
 import { Route } from 'react-router-dom';
 import { Login, Signup, Main, PostWrite, Detail } from '../pages';
+import Post from '../components/Post';
 
 function App() {
   return (
@@ -14,7 +14,10 @@ function App() {
           <Route path='/login' exact component={Login} />
           <Route path='/register' exact component={Signup} />
           <Route path='/postWrite' exact component={PostWrite} />
+          {/* Post컴포넌트 확인용 */}
+          <Route path='/post' exact component={Post} />
           <Route path='/postWrite/:num' exact component={PostWrite} />
+          {/* detail에서 detail/:num으로 바꿀예정 */}
           {/* <Route path='/detail/:num' exact component={Detail} /> */}
           <Route path='/detail' exact component={Detail} />
         </ConnectedRouter>
