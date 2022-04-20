7c37265cf367d80d669be894d1d60251f689a402
hyoungwonkang <hwplus@gmail.com>
2022-04-20 09:21:34 +0900

    포스트만 맵핑해주는 역할로 바뀜

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import Post from './Post';
-import { Grid, Text, Image } from '../elements/index';
 import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import { actionCreators as postActions } from '../redux/modules/post';
 
 const PostList = (props) => {
+  const list = useSelector((state) => state?.post?.list?.posts);
+  console.log(list);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(postActions.getMainDB());
+  }, []);
+
   return (
-    <VideoList>
-      <>
-        <Post />
-        <Parent>
-          <Image shape='profile' src={props.src} margin='5px 20px 0 0' />
-          <TitleBox>
-            <Text margin='0' size='16px' weight='700' color='#fff'>
-              {/* 제목이 있는곳 */}
-              {props.postTitle}
-            </Text>
-            <Text margin='3px 0' size='14px' color='#aaa' weight='500'>
-              {/* 항해 클론 5조 */}
-              {props.userNick}
-            </Text>
-            <Text margin='0' color='#aaa' size='14px'>
-              {/* 조회수 100만회 &nbsp; 3일 전 */}
-              {props.postDate}
-            </Text>
-          </TitleBox>
-        </Parent>
-      </>
-      <Post />
-      <Post />
-      <Post />
-    </VideoList>
+    <React.Fragment>
+      {list ? (
+        list.map((post, i) => {
+          return (
+            <VideoList>
+              <Post {...post} />
+            </VideoList>
+          );
+        })
+      ) : (
+        <div>아무것도 없음</div>
+      )}
+    </React.Fragment>
   );
 };
 
@@ -39,26 +36,4 @@ const VideoList = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
 `;
 
-const Profile = styled.div`
-  display: flex;
-  width: 40px;
-  height: 40px;
-  border-radius: 70%;
-  margin: 5px 20px 0 0;
-  background-color: #aaa;
-`;
-const Parent = styled.div`
-  display: flex;
-  align-items: flex-start;
-  height: 50px;
-  margin: 10px 0 0;
-`;
-const TitleBox = styled.div`
-margin: 5px;
-font-size: 1em;
-font-weigth: bold;
-left: 30%,
-display: inline-block;
-width: 70%
-`;
 export default PostList;
