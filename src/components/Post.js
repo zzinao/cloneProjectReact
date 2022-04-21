8b56e59dbcf9f52caf68b56639fc7f7378e2cdfa
hyoungwonkang <hwplus@gmail.com>
2022-04-21 17:16:48 +0900

    머지전 커밋 형원

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,42 +1,57 @@
-import React from 'react'
-import { Grid, Text, Image } from '../elements/index'
-import { history } from '../redux/configureStore'
-import { changeTime } from '../shared/ChangeTime'
-import ReactPlayer from 'react-player/lazy'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import { Grid, Text, Image } from '../elements/index';
+import { history } from '../redux/configureStore';
+import { changeTime } from '../shared/ChangeTime';
+import ReactPlayer from 'react-player/lazy';
+import styled from 'styled-components';
 const Post = (props) => {
+  const [hover, setHover] = useState(false);
   return (
     <>
       <Container
         onClick={() => {
-          history.push(`/detail/${props.postNum}`)
+          history.push(`/detail/${props.postNum}`);
         }}
       >
-        <Image shape="rectangle" src_01={props.postThumb} />
-        {/* <Preview src={props.postThumb} /> */}
-        {/* <ReactPlayer url={props.postVideo} playing={false} muted={false} /> */}
+        <div
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+        >
+          {hover ? (
+            <ReactPlayer
+              url={props.postVideo}
+              playing={true}
+              muted={true}
+              width='293px'
+              height='170.5px'
+              style={{ margin: '0px 0px 15px 0px' }}
+            />
+          ) : (
+            <Image shape='rectangle' src_01={props.postThumb} />
+          )}
+        </div>
         <Parent>
           <Image
-            shape="profile"
+            shape='profile'
             src_01={props.userInfo.userProfile}
-            margin="5px 20px 0 0"
+            margin='5px 20px 0 0'
           />
           <TitleBox>
-            <Text margin="0" size="16px" weight="700" color="#fff">
+            <Text margin='0' size='16px' weight='700' color='#fff'>
               {props.postTitle}
             </Text>
-            <Text margin="3px 0" size="14px" color="#aaa" weight="500">
+            <Text margin='3px 0' size='14px' color='#aaa' weight='500'>
               {props?.userInfo?.userId}
             </Text>
-            <Text margin="0" color="#aaa" size="14px">
+            <Text margin='0' color='#aaa' size='14px'>
               {props.postCnt} &nbsp; {changeTime(props.postDate)}
             </Text>
           </TitleBox>
         </Parent>
       </Container>
     </>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   width: 293px;
@@ -45,27 +60,21 @@ const Container = styled.div`
   padding: 1rem;
   min-height: 170px;
   flex-direction: column;
-`
-
-const Preview = styled.div`
-  width: 100%;
-  height: 180px;
-  background-color: #aaa;
-`
+`;
 
 const Parent = styled.div`
   display: flex;
   align-items: flex-start;
   height: 50px;
   margin: 10px 0 0;
-`
+`;
 const TitleBox = styled.div`
-margin: 5px;
-font-size: 1em;
-font-weigth: bold;
-left: 30%,
-display: inline-block;
-width: 70%
-`
+  margin: 5px;
+  font-size: 1em;
+  font-weight: bold;
+  left: 30%;
+  display: inline-block;
+  width: 70%;
+`;
 
-export default Post
+export default Post;
